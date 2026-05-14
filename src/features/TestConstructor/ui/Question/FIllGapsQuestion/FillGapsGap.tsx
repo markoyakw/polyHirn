ddecd21770e0179fa518aa0a41c5c790b5551be9
Marko Yakovenko <yakovenkomarko@gmail.com>
2026-05-14 15:23:24 +0200

    fix the fill gaps question gap resizing handles positioning

--- src/features/TestConstructor/ui/Question/FIllGapsQuestion/FillGapsGap.tsx
+++ src/features/TestConstructor/ui/Question/FIllGapsQuestion/FillGapsGap.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, type FC, type PointerEvent } from "react"
+import { useEffect, useLayoutEffect, useRef, useState, type FC, type PointerEvent } from "react"
 import type { TFillGapsGap, TFillGapsGapResizeSide } from "./utils"
 import { BiSolidLeftArrow } from "react-icons/bi"
 import classes from "./FillGapsQuestion.module.css"
@@ -27,20 +27,34 @@ const FillGapsGap: FC<TFillGapsGapProps> = ({
     }
 
     const [isResizing, setIsResizing] = useState(false)
+    const gapRef = useRef<HTMLSpanElement | null>(null)
 
     useEffect(() => {
+        //resizing state to hide resize handles on gap size change
         const handlePointerUp = () => {
             setIsResizing(false)
         }
-
         window.addEventListener("pointerup", handlePointerUp)
         return () => window.removeEventListener("pointerup", handlePointerUp)
     }, [])
+
+    useLayoutEffect(() => {
+        //calculating last row right for right resize handle positioning
+        const gap = gapRef.current
+        if (!gap) return
+        const rects = gap.getClientRects()
+        const lastRowRect = rects[rects.length - 1]
+        const firstRowRect = rects[0]
+        const firstRowRelativeLeft = firstRowRect.left - lastRowRect.left
+        const { width: lastRowWidth } = lastRowRect
+        gap.style.setProperty("--last-row-right", lastRowWidth - firstRowRelativeLeft + "px")
+    }, [gap])
 
     const gapClassName = clsx(classes["gap"], isResizing && classes["gap--resizing"])
 
     return (
         <span
+            ref={gapRef}
             className={gapClassName}
             data-gap-id={gap.id}
             data-gap-start={gap.start}
